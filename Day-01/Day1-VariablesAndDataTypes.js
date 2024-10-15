6551543a9fa027b4eb59cc308b32c44a30d0479d
Karthik-Balli <karthikballi0001@gmail.com>
2024-10-15 20:19:34 +0530

    Day-01 Challenge completed #01

---
+++ Day-01/Day1-VariablesAndDataTypes.js
@@ -0,0 +1,39 @@
+//  Task1
+var num = 25;
+console.log(num);
+
+// Task2
+let str = "Hello JavaScript";
+console.log(str);
+
+// Task3
+const bool = true;
+console.log(bool);
+
+let num1 = 10;
+let str1 = "Hello Test"
+let bool1 = false
+let obj = {Id: 1, Name: "John"}
+let arr = [1, 2, 3, 4, 5]
+
+console.log(typeof num1);
+console.log(typeof str1);
+console.log(typeof bool1);
+console.log(typeof obj);
+console.log(typeof arr);
+
+
+// Task5 reassigning variables
+let var1 = 13;
+console.log(var1); // 13
+
+var1 = 23;
+console.log(var1);  // 23
+
+// Task6 understanding const
+const greet = "Good Morning"
+console.log(greet)
+
+// greet = "Good Afternoon"
+// console.log(greet)  // TypeError: Assignment to constant variable.
+
