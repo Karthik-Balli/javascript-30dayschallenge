dea638a7104dd536af947f1424860cc4fcbbc815
Karthik-Balli <karthikballi0001@gmail.com>
2024-10-16 23:01:44 +0530

    Day-02 Challenge Completed #01

---
+++ Day-02/Day2-Operators.js
@@ -0,0 +1,59 @@
+let num1 = 10;
+let num2 = 15;
+
+// Task1 - Add two numbers
+let sum = num1 + num2;
+console.log("Sum: ", sum);
+
+// Task2 - Sub two numbers
+let sub = num2 - num1;
+console.log("Sub: ", sub);
+
+
+// Task3 - Mul two numbers
+let mul = num1 * num2;
+console.log("Mul: ", mul);
+
+// Task4 - Div two numbers
+let div = num2 / num1;
+console.log("Div: ", div);
+
+// Task5 - Get rem from two numbers
+let rem = num2 % num1;
+console.log("Rem: ", rem);
+
+// Task6 - += operator
+let addAss = 1;
+addAss += 10;
+console.log("AddAss: ", addAss);
+
+// Task7 - -= Operator
+let subAss = 30;
+subAss -= 10;
+console.log("SubAss: ", subAss);
+
+// Comparison Operators
+
+// Task8, Task9, Task10
+let lessOp = num1 < num2;
+console.log(lessOp);
+
+let greaterOp = num1 > num2;
+console.log(greaterOp);
+
+let lessEqOp = num1 <= num2;
+console.log(lessEqOp);
+
+let greaterEqOp = num1 >= num2;
+console.log(greaterEqOp);
+
+let doubleEq = num1 == 10;
+console.log(doubleEq);
+
+let trippleEq = num1 === 10;
+console.log(trippleEq);
+
+// Task11 - Trenary Operator
+// To find out given number is positive or negative number
+let checkNumber = num1 > 0 ? "Positive Number" : "Negative Number";
+console.log(checkNumber);
